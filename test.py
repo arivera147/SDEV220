5e897934f074509b40d6bd3643f9c37fe97a09b9
Amanda Rivera <arivera147@ivytech.edu>
2025-02-14 13:48:33 -0500

    Adding Files

---
+++ test.py
@@ -0,0 +1,27 @@
+#run in terminal: C:\Users\AmaRi\project
+
+import unittest
+
+from my_sum import sum #my_sum is a developer-created package
+from fractions import Fraction #fractions is a part of standrard library
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1,4), Fraction(1,4), Fraction (2,5)]
+        result = sum(data)
+        self.assertEqual(result,1)
+
+if __name__ == '__main__':
+    unittest.main()
